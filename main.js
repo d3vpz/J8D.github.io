5f23b7156d21815aad25bda4f34e2a22855b2f5e
Devpz <114014131+Devpooldotmsi@users.noreply.github.com>
2023-10-09 08:47:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -9,8 +9,6 @@ let x = 50
 function loop() {
     j8_clearRect(0,0,256,240);
     j8_setBGColor("lightskyblue");
-    j8_lineWidth=1;
-    j8_drawText(25,50,"20px Arial","white","Wolrds Hardst Gaem :o");
     j8_lineWidth=2;
     j8_ofRect(x,y,10,10,"maroon","red");
 
